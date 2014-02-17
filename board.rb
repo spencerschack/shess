b1de0d62c14bac794c7a8be2998ddb843cda277c
Spencer Schack <scs1818@gmail.com>
2014-02-16 18:13:08 -0800

    cpu

--- board.rb
+++ board.rb
@@ -1,22 +1,26 @@
 class Board
 
-	attr_reader :board, :from, :to
+	ASCII = {
+		king:   "\u265A",
+		queen:  "\u265B",
+		rook:   "\u265C",
+		bishop: "\u265D",
+		knight: "\u265E",
+		pawn:   "\u265F"
+	}
+
+	attr_reader :uci
 
 	def initialize
-		@board = [
-			[  Rook, Knight, Bishop,  Queen,   King, Bishop, Knight,   Rook],
-			[  Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn],
-			[   nil,    nil,    nil,    nil,    nil,    nil,    nil,    nil],
-			[   nil,    nil,    nil,    nil,    nil,    nil,    nil,    nil],
-			[   nil,    nil,    nil,    nil,    nil,    nil,    nil,    nil],
-			[   nil,    nil,    nil,    nil,    nil,    nil,    nil,    nil],
-			[  Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn,   Pawn],
-			[  Rook, Knight, Bishop,  Queen,   King, Bishop, Knight,   Rook]
-		]
-		board[0..1].each{|row| row.map!{|klass| klass.new(self, :white) } }
-		board[6..7].each{|row| row.map!{|klass| klass.new(self, :black) } }
-		@from = Coordinate.new(0, 0)
-		@to = Coordinate.new
+		@uci = Uci.new(engine_path: '/usr/local/bin/stockfish')
+		sleep(1) while !@uci.ready?
+	end
+
+	def play
+		loop do
+			draw
+			uci.go!
+		end
 	end
 
 	def draw
@@ -26,73 +30,31 @@ class Board
 			@printed = true
 		end
 		puts ''
-		board.each_with_index do |row, y|
+		(0..7).each do |y|
 			print '  '
-			row.each_with_index do |piece, x|
-				print "\e[48;5;#{color_for(x, y)}m"
-				print piece ? piece.to_s : ' '
+			(0..7).each do |x|
+				print color_at(x, y)
+				print piece_at(x, y)
 				print ' '
 			end
 			puts "\e[0m"
 		end
 	end
 
-	def enter
-		if to.valid?
-			move from, to
+	def piece_at x, y
+		x = ('a'..'h').to_a[x]
+		y = 8 - y
+		pos = "#{x}#{y}"
+		if uci.piece_at?(pos)
+			piece, color = uci.get_piece(pos)
+			"\e[38;5;#{color == :white ? 231 : 16}m#{ASCII[piece]}"
 		else
-			to.move_to(from)
+			' '
 		end
 	end
 
-	def up
-		cursor.y -= 1
-	end
-
-	def down
-		cursor.y += 1
-	end
-
-	def right
-		cursor.x += 1
-	end
-
-	def left
-		cursor.x -= 1
-	end
-
-	def [] coord
-		board[coord.y][coord.x]
-	end
-
-	def []= coord, value
-		board[coord.y][coord.x] = value
-	end
-
-	private
-
-	def cursor
-		to.valid? ? to : from
-	end
-
-	def move from, to
-		if self[from] && self[from].move(from, to)
-			from.move_to(to)
-			to.reset
-		end
-	end
-
-	def color_for x, y
-		case
-		when to.at?(x, y)
-			202
-		when from.at?(x, y)
-			27
-		when (x + y).odd?
-			238
-		else
-			244
-		end
+	def color_at x, y
+		"\e[48;5;#{(x + y).odd? ? 238 : 244}m"
 	end
 
 end
--- shess.rb
+++ shess.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 
 require 'io/console'
+require 'uci'
 require 'pry'
 require_relative 'coordinate.rb'
 require_relative 'board.rb'
@@ -12,27 +13,4 @@ require_relative 'pieces/knight.rb'
 require_relative 'pieces/rook.rb'
 require_relative 'pieces/pawn.rb'
 
-board = Board.new
-loop do
-	board.draw
-	STDIN.noecho do |io|
-		io.raw do |io|
-			input = io.getch
-			input << io.read_nonblock(2) rescue nil
-			case input
-			when "\e[A"
-				board.up
-			when "\e[B"
-				board.down
-			when "\e[C"
-				board.right
-			when "\e[D"
-				board.left
-			when "\r"
-				board.enter
-			else
-				exit
-			end
-		end
-	end
-end
+Board.new.play
